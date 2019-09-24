33dd40cd470bccb966434dc475b6b6c1b22e403e
supano <supano1995@gmail.com>
2019-09-24 19:35:03 +0700

    [Update] add user route

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo"
 )
+
+type user struct {
+	Name     string `json:"name" form:"name" query:"name"`
+	Email    string `json:"email" form:"email" query:"email"`
+	Password string `json:"password" form:"password" query:"password"`
+}
 
 func main() {
 	e := echo.New()
@@ -12,5 +19,19 @@ func main() {
 		return c.String(http.StatusOK, "Hello world")
 	})
 
+	e.POST("/users", func(c echo.Context) error {
+
+		u := new(user)
+		if err := c.Bind(u); err != nil {
+			return err
+		}
+
+		return c.JSON(http.StatusOK, u)
+	})
+
 	e.Logger.Fatal(e.Start(":9000"))
+}
+
+func addUser(name string, email string, password string) {
+	fmt.Println(name)
 }
